decc023028d123187999d975644e9c496fbcb2f5
kate <you@example.com>
2020-01-04 11:23:21 +0100

    next commit

--- src/Conversation.java
+++ src/Conversation.java
@@ -2,8 +2,8 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class Conversation {
-    public static final int MIN_VECTOR_BOUND = -10;
-    public static final int MAX_VECTOR_BOUND = 10;
+    public static final int MIN_VECTOR_BOUND = -50;
+    public static final int MAX_VECTOR_BOUND = 50;
 
     Scanner in = new Scanner(System.in);
     int vectorsNumber;
@@ -12,7 +12,7 @@ public class Conversation {
     Vector[] vectors;
 
     public void startdialogwithUser() {
-        System.out.println("Vektor Rechner");
+        System.out.println("VEKTOR RECHNER");
         howManyVectorsQuestion();
         askIsRandom();
         askVectorsDimension();
@@ -23,10 +23,10 @@ public class Conversation {
         for (int i = 0; i < vectors.length; i++) {
             System.out.println("Vector " + (i + 1) + ": " + vectors[i]);
         }
-        hauptmenu();
+        menuOperationen();
     }
 
-    public void howManyVectorsQuestion() {
+    private void howManyVectorsQuestion() {
         System.out.println("Mit wie viele Vektoren möchten Sie arbeiten? (min.3)");
         int howManyVectors = in.nextInt();
         this.vectorsNumber = howManyVectors;
@@ -38,7 +38,7 @@ public class Conversation {
 
     }
 
-    public void askIsRandom() {
+    private void askIsRandom() {
         System.out.println("Möchten Sie mit Zufallvektoren arbeiten? j/n");
         String answer = in.next();
         if (answer.equals("j")) {
@@ -48,7 +48,7 @@ public class Conversation {
         }
     }
 
-    public void askVectorsDimension() {
+    private void askVectorsDimension() {
         dimensions = new int[vectorsNumber];
         for (int i = 0; i < vectorsNumber; i++) {
             int dimension = askDimension(i);
@@ -71,7 +71,7 @@ public class Conversation {
         vectors = new Vector[vectorsNumber];
         for (int i = 0; i < dimensions.length; i++) {
             int currentDimension = dimensions[i];
-            int[] content = new int[currentDimension];
+            float[] content = new float[currentDimension];
             for (int j = 0; j < currentDimension; j++) {
                 System.out.printf("Geben Sie Zahl %d für Vektor %d%n", j + 1, i + 1);
                 content[j] = in.nextInt();
@@ -86,7 +86,7 @@ public class Conversation {
         vectors = new Vector[vectorsNumber];
         for (int i = 0; i < dimensions.length; i++) {
             int currentDimension = dimensions[i];
-            int[] content = new int[currentDimension];
+            float[] content = new float[currentDimension];
             for (int j = 0; j < currentDimension; j++) {
                 content[j] = getRandomNumberInRange(MIN_VECTOR_BOUND, MAX_VECTOR_BOUND);
             }
@@ -95,28 +95,6 @@ public class Conversation {
         }
     }
 
-    public void hauptmenu() {
-        System.out.println("---------------HAUPTMENU----------------");
-        char wahl = '0';
-        while (wahl != 'e') {
-        System.out.println("--> Bitte wählen Sie eine Aktion aus <--");
-        System.out.println(" 1 - Matematische Operationen mit Vektoren durchführen ");
-        System.out.println(" 2 - Vektoren ändern");
-        System.out.println(" e - Programm beenden");
-        System.out.println("Ihre Eingabe: ");
-        wahl = in.next().charAt(0);}
-        switch (wahl) {
-            case '1':
-                menuOperationen();
-                break;
-            case '2':
-                menuChanges();
-                break;
-            case 'e':
-            default:
-                System.out.println("Falsche Eingabe - bitte wiederholen");
-        }
-    }
 
     public void menuOperationen() {
         System.out.println("-----------MENU: OPERATIONEN------------");
@@ -131,8 +109,7 @@ public class Conversation {
             System.out.println(" m - Ein Vektor mit einer Zahl multipliezieren");
             System.out.println(" i - Einheitsvektor berechnen");
             System.out.println(" l - Länge des Vektors berechnen");
-            System.out.println(" r - Spatprodukt berechnen");
-            System.out.println(" z - zurück zum Hauptmenu");
+            //System.out.println(" r - Spatprodukt berechnen");
             System.out.println(" e - Programm beenden");
             System.out.println("Ihre Eingabe: ");
             aktion = in.next().charAt(0);
@@ -158,39 +135,13 @@ public class Conversation {
                 case 'l':
                     betragDialog();
                     break;
-                case 'r':
-                    spatDialog();
-                case 'z':
-                    hauptmenu();
-                    break;
+                //case 'r':
+                //   spatDialog();
                 case 'e':
                     break;
                 default:
                     System.out.println("Falsche Eingabe - bitte wiederholen");
             }
-        }
-    }
-
-    public void menuChanges() {
-        System.out.println("-----------ÄNDERUNGEN VORNEHMEN-----------");
-        char choice = '0';
-        while (choice != 'e') {
-        System.out.println("--> Bitte wählen Sie eine Aktion aus <--");
-        System.out.println(" 1 - Vektor ändern ");
-        System.out.println(" 2 - Mit neuen Vektoren starten");
-        System.out.println(" z - zurück zum Hauptmenu");
-        System.out.println(" e - Programm beenden");
-        System.out.println("Ihre Eingabe: ");
-        choice = in.next().charAt(0);}
-        switch (choice) {
-            case '1':
-            case '2':
-            case 'z':
-                hauptmenu();
-                break;
-            case 'e':
-            default:
-                System.out.println("Falsche Eingabe - bitte wiederholen");
         }
     }
 
@@ -203,13 +154,17 @@ public class Conversation {
 
     public void summeDialog() {
         System.out.println("Wählen Sie Vektor - Summand 1 (bsp. 1)");
-        System.out.println("Bitte beachten dass die Vektoren sollen gleiche Dimensionen haben");
+        System.out.println("Bitte beachten Sie dass die Vektoren sollen gleiche Dimensionen haben");
         int s1 = in.nextInt();
         System.out.println("Wählen Sie Vektor - Summand 2 (bsp. 2)");
-        System.out.println("Bitte beachten dass die Vektoren sollen gleiche Dimensionen haben");
+        System.out.println("Bitte beachten Sie dass die Vektoren sollen gleiche Dimensionen haben");
         int s2 = in.nextInt();
         Vector a = vectors[s1 - 1];
         Vector b = vectors[s2 - 1];
+        if (dimensions[s1 - 1] != dimensions[s2 - 1]) {
+            System.out.println("Die Vektoren sollen gleiche Dimension haben - wählen noch einmal");
+
+        }
         Vector c = a.sum(b);
         System.out.printf("Summe von Vektor %d und Vektor %d ist:%n", s1, s2);
         System.out.println(a + " + " + b + " = " + c);
@@ -241,7 +196,7 @@ public class Conversation {
         int mp2 = in.nextInt();
         Vector a = vectors[mp1 - 1];
         Vector b = vectors[mp2 - 1];
-        int c = a.skalarprodukt(b);
+        float c = a.skalarprodukt(b);
 
         System.out.printf("Skalarprodukt von Vektor %d und Vektor %d ist:%n", mp1, mp2);
         System.out.println(a + " * " + b + " = " + c);
@@ -271,12 +226,12 @@ public class Conversation {
         System.out.println("Wählen Sie Vektor für Berechnen (bsp. 1)");
         int be = in.nextInt();
         Vector a = vectors[be - 1];
-        int b = a.betrag();
+        float b = a.betrag();
         System.out.printf("Länge des Vektors %d ist: ", be);
         System.out.println(b);
     }
 
-    public void spatDialog() {
+   /* public void spatDialog() {
         System.out.println("Wählen Sie den ersten Vektor aus");
         int v1 = in.nextInt();
         Vector a = vectors[v1 - 1];
@@ -287,7 +242,8 @@ public class Conversation {
         int v3 = in.nextInt();
         Vector c = vectors[v3 - 1];
         int det =
-    }
+    }*/
+
 
     private static int getRandomNumberInRange(int min, int max) {
         Random r = new Random();
